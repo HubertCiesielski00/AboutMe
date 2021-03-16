597b8f08706fef0c1e43e546ba1e8f62e4cef47e
Hubert Ciesielski <hubert.ciesielski@onet.pl>
2021-03-16 12:48:08 +0100

    -Deleted unneded components.
    -Added animations to sections.
    
    -Added inrpolated sections names
    
    -App structure remade

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
-import { faIdBadge, faIdCard } from '@fortawesome/free-regular-svg-icons';
+import { Component, OnInit } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faIdBadge, faCheckSquare } from '@fortawesome/free-regular-svg-icons';
+import { faBook, faBookReader } from '@fortawesome/free-solid-svg-icons';
+
 import { browser } from 'protractor';
 import { BrowserStack } from 'protractor/built/driverProviders';
 import { Browser } from 'selenium-webdriver';
@@ -12,4 +15,15 @@ import { Browser } from 'selenium-webdriver';
 })
 export class AppComponent {
   title = 'AboutMe';
+  faBook = faBookReader;
+  faCheck = faCheckSquare;
+  faBadge = faIdBadge;
+
+  Sections = ['O mnie','Moje umiejętności','Plany rozwoju'];
+
+  templateFunc(ToShow: HTMLElement, ToHide:HTMLElement, ToHide2: HTMLElement ){
+    ToShow.classList.remove("section__hidden");
+    ToHide.classList.add("section__hidden");
+    ToHide2.classList.add("section__hidden");
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { HeaderComponent } from './header/header.component';
-import { AboutMeComponent } from './about-me/about-me.component';
-import { MySkillsComponent } from './my-skills/my-skills.component';
-import { LearningComponent } from './learning/learning.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    AboutMeComponent,
-    MySkillsComponent,
-    LearningComponent,
   ],
   imports: [
     BrowserModule,
